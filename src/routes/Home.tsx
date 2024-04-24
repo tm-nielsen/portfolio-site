e2c8ade9a958527876aa6467145e732cb6c62612
Taylor Nielsen <littleb88888888@gmail.com>
2024-04-24 13:24:40 -0600

    Added initial home page content and styling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,23 @@
+import profile from '../assets/thumb_pfp.png'
+
 const Home = () => {
   return (
-    <>Home</>
+    <>
+      <div className="flex-row">
+        <img src={profile} alt="placeholder identification picture" />
+        <div className='flex-column'>
+          <p>Hey there, I'm</p>
+          <h1>Twig Nielsen</h1>
+          <p>
+            a capable developer passionate about creating engaging and accessible experiences.
+            Most of the projects I've made and learned from have been games, but I've built few compentent websites along the way using React.
+          </p>
+        </div>
+      </div>
+      <>
+        <h2>Some Things I Know</h2>
+      </>
+    </>
   )
 }
 
