6b479b0a5e437666395bf8dfc9022733bd2eb399
Twig Nielsen <littleb88888888@gmail.com>
2024-04-28 23:44:42 -0400

    Added initial sketch of games page

--- src/routes/Games.tsx
+++ src/routes/Games.tsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react"
+
+interface GameInfo {
+  title: string
+}
+
 const Games = () => {
+  const [gameList, setGameList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/games')
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      setGameList(data.games)
+      console.log(data)
+    })
+  }, [])
+
   return (
-    <>Games</>
+    <>
+      <h1>Games</h1>
+      <ul>
+        {gameList? gameList.map((gameInfo: GameInfo, index) => {
+          return <li key={index}>{gameInfo.title}</li>
+        }): null}
+      </ul>
+    </>
   )
 }
 
