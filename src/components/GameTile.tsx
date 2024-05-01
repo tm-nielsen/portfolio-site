fb0c6cb4a2a125e111cfc7c66dfd5127cf87a959
Twig Nielsen <littleb88888888@gmail.com>
2024-04-30 23:45:21 -0400

    Updated game tile styling

--- src/components/GameTile.tsx
+++ src/components/GameTile.tsx
@@ -2,13 +2,13 @@ import { FC } from "react"
 
 const GameTile = (gameInfo: GameInfo, key: number) => {
   return (
-    <div className='game-tile' key={key}>
+    <a className='game-tile' key={key} href={gameInfo.url} target='_blank'>
       <img src={gameInfo.cover_url} alt="" className='game-cover' />
       <div className='game-info'>
         <h3 className='game-title'>{gameInfo.title}</h3>
         {/* <p>{gameInfo.short_text}</p> */}
       </div>
-    </div>
+    </a>
   )
 }
 
