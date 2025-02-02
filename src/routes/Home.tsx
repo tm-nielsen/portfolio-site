6dca9c937da607d3620dd18fc8fad2741866ba78
Taylor Nielsen <littleb88888888@gmail.com>
2025-02-02 15:01:01 -0700

    Reworked home page - swapped out collapsible resume sections with a simple space for featured projects - commented out link to download CV - removed source file for resume filler

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
-import RevealableSection from '../components/RevealableSection'
 import profile from '../assets/profile_picture.png'
-import knowledge from '../assets/knowledge.json'
+import featuredProjects from '../assets/featured_projects.json'
 import '../styles/home.css'
 
 export default function Home() {
@@ -11,7 +10,7 @@ export default function Home() {
         <img src={profile} alt="placeholder identification picture" className='profile-picture' />
         <div className='flat-bottom'>
           <p>Hey there, I'm</p>
-          <h1>Twig Nielsen</h1>
+          <h1>Twig</h1>
           <i className='subscript'>
             <p className='centred-text'>B.Sc. in Software Engineering</p>
             <p className='centred-text'>(she/her)</p>
@@ -25,56 +24,23 @@ export default function Home() {
         <a href="http://github.com/tm-nielsen" target="_blank" className='centred-text'>
           Github: tm-nielsen
         </a>
-        <a href="Twig Nielsen CV.pdf" download={true} className='centred-text'>
+        {/* <a href="Twig Nielsen CV.pdf" download={true} className='centred-text'>
           Download my CV
-        </a>
+        </a> */}
       </div>
-      <p className='games-link-body'>
-        You should check out my <Link to='/games' className='flat'>Games</Link>
-      </p>
-      <RevealableSection title="About Me">
-        <p>
-          I'm a capable developer passionate about creating engaging and accessible experiences. Most of the projects I've made and learned from have been games, but I've built few competent websites along the way using React.
-        </p>
-      </RevealableSection>
-      <RevealableSection title="Skills I have Acquired">
-        <ul className='flat-list'>
-          {knowledge.skills.map((skillInfo, index) => {
-            return (
-            <li key={index}>
-              <RevealableSection title={skillInfo.name} HeadingLevel='h3' contentPadding={0.5}>
-                <p className='flat'>{skillInfo.description}</p>
-              </RevealableSection>
-            </li>
-            )
-          })}
-        </ul>
-      </RevealableSection>
-      <RevealableSection title="Technologies I am Familiar With">
-        <ul className='row'>
-          {knowledge.technologies.map((entry, index) =>
-            <li key={index}>
-              <p className='flat'>{entry}</p>
-            </li>
-          )}
-        </ul>
-      </RevealableSection>
-      <RevealableSection title="Programming Languages I Know">
-        {knowledge.languages.map((languageTier, index) => {
-          return (
-            <div className='flat' key={index}>
-              <h3>{languageTier.tierName}</h3>
-              <ul className='row'>
-                  {languageTier.languages.map((language, index) =>
-                    <li key={index}>
-                      <p className='flat'>{language}</p>
-                    </li>
-                  )}
-              </ul>
-            </div>
-          )
-        })}
-      </RevealableSection>
+      <p className='professional-summary'>...a capable developer passionate about creating engaging and accessible experiences. Most of the projects I've made and learned from have been games, but I've built few competent websites along the way using React.</p>
+      <h2 className='featured'>Featured Projects</h2>
+      <ul className='flat'>
+        {featuredProjects.map(({title, link, description}, index) =>
+          <div key={index}>
+            <Link to={link} className='featured'>
+              <h3 className='flat centred-text'>{title}</h3>
+            </Link>
+            <p>{description}</p>
+          </div>
+        )}
+      </ul>
+      <Link to='/games' className='games-link'>More Games</Link>
     </>
   )
 }
