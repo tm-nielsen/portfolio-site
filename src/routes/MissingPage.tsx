7841e9fb1b35a1e7cce6b9f67e614c481480fea0
Twig Nielsen <littleb88888888@gmail.com>
2025-09-16 09:45:24 -0600

    Added root link to missing page

--- src/routes/MissingPage.tsx
+++ src/routes/MissingPage.tsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom"
+
 export default function MissingPage() {
   return (
-    <>page does not exist</>
+    <>
+      <>page does not exist</>
+      <p><Link to="/">Home</Link></p>
+    </>
   )
 }
