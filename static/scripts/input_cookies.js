c7b907c26d1a46fbe019c19839aaf972489a5406
Twig Nielsen <littleb88888888@gmail.com>
2025-12-05 12:26:38 -0700

    Implemented cookieStore alternatives - added alternative `cookieStore` methods for older browsers

--- static/scripts/input_cookies.js
+++ static/scripts/input_cookies.js
@@ -3,6 +3,30 @@ const textInputs = document.querySelectorAll('input[type="text"]')
 
 checkboxes.forEach(checkbox => checkbox.removeAttribute('disabled'))
 
+
+const cookieStoreSupported = typeof(cookieStore)!="undefined"
+
+async function getCookie(label) {
+    if (cookieStoreSupported) {
+        return cookieStore.get(label)
+    }
+    else {
+        const cookieRegex = RegExp(`${label}=([^;])+`)
+        const regexResult = document.cookie.match(cookieRegex)
+        return regexResult[1]
+    }
+}
+
+function SetCookie(label, value) {
+    if (cookieStoreSupported) {
+        cookieStore.set(label, value)
+    }
+    else {
+        document.cookie = `${label}=${value}`
+    }
+}
+
+
 function bindCookieMethods(inputElement, setMethod, getMethod) {
     const labelText = inputElement.parentElement.textContent
     const cookieLabel = labelText.split('\n')[0].trim()
